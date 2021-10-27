d56bf0b81f695e347824007c0325d4bc675e0a04
Alexis <alexis.escudero195@gmail.com>
2021-10-27 09:51:50 +0200

    evolution tp1 jusqu'a partie 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,37 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
+
+
+
 export class AppComponent {
   title = 'TestTP1partie1';
+  seance = 'Séance Tp1';
+  resumeSeance = "Prise en main d'angular, première applciation";
+  nomEtu = "Jean";
+  prenomEtu = "Martin";
+  retNomEtu(){
+    return "Jean";
+  }
+
+  retPrenomEtu(){
+    return "Martin";
+  }
+
+  estAuth = true;
+
+  
+  
+  constructor() {
+
+    this.estAuth = false;
+    
+    setTimeout(function(){
+      this.estAuth = false;
+    },3000);
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EtudiantComponent
   ],
   imports: [
     BrowserModule
